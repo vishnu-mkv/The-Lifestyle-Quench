5ceff8e3240f7e5adbaa8c8bd486b60640d64a29
Vishnu MK <mkv1722@gmail.com>
2021-05-30 11:25:19 +0530

    Django secret key changed and moved to secrets.py
    Signed-off-by: Vishnu MK <mkv1722@gmail.com>

--- server/thelifestylequench/settings.py
+++ server/thelifestylequench/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from server.thelifestylequench.secrets import DJANGO_SECRET_KEY
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5-1%xq0xefra1r8=(j0(+zd@x1cwnb-$)=thwii=t$99w&#cm)'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
